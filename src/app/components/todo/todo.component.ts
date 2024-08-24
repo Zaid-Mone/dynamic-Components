f6d9c04f984a8c7b7d27ce174840e31b6d7e4aeb
Zaid-Mone <zaidmone9@gmail.com>
2024-08-24 18:02:25 +0300

    add CheckBox

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -2,17 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { APIService } from '../../services/api.service';
 import { Todo } from '../../models/Todo';
 import { DTableComponent } from '../../core/d-table/d-table.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [DTableComponent],
+  imports: [DTableComponent,FormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent implements OnInit{
   todos:Todo[]=[]
   totalRecords=200;
+  selectAll:boolean=false;
+  selectedItems: any[] = [];
   constructor(private service:APIService) {}
 
   ngOnInit(): void {
@@ -46,6 +49,18 @@ export class TodoComponent implements OnInit{
     this.getToDo(event.page,event.pageSize);
    }
 
-
-
+   toggleSelectAll(event: any): void {
+    debugger;
+    this.selectAll = (event.target as HTMLInputElement).checked;
+  }
+  toggleSelection(item: any, event: Event): void {
+    debugger;
+    item.selected = (event.target as HTMLInputElement).checked;
+    if (item.selected) {
+      this.selectedItems.push(item);
+    } else {
+      this.selectedItems = this.selectedItems.filter(selectedItem => selectedItem !== item);
+    }
+    // this.selectAll = this.tableData.every(item => item.selected);
+  }
 }
